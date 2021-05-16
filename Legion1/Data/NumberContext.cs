9f0a1a5e05d3db1b606e4fb46c7732109a94fdcf
ArralS <80539257+ArralS@users.noreply.github.com>
2021-05-16 20:45:19 +0200

    Pliki Początkowe

---
+++ Legion1/Data/NumberContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Legion1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace Legion1.Data
+{
+     public class NumberContext : IdentityDbContext<IdentityUser>
+    {
+        public NumberContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Recent> Recent { get; set; }
+        
+        }
+
+    
+}
---
+++ Legion1/Models/Recent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Legion1.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Legion1.Models
+{
+    public class Recent
+    {
+        public int ID { get; set; }
+        public String Result { get; set; }
+        
+        public int Number { get; set; }
+       
+        public DateTime Data { get; set; }
+
+        public String Owner { get; set; }
+
+
+    }
+}
